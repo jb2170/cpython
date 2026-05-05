2a96519e85487db00730b37bfa368ce70b55e7ae
jb2170 <email@jb2170.com>
2026-05-05 16:14:10 +0100

    argparse subnamespace: Replace '-'s with '_'s in subnamespace destintation name
    Like in `_ActionsContainer._get_optional_kwargs`. It makes it much
    easier to access the subnamespaces by `x.y` instead of needing
    `getattr(x, 'y')` in the case that y (the subparser choice) has hyphens
    in it.

--- Lib/argparse.py
+++ Lib/argparse.py
@@ -1402,6 +1402,7 @@ class _SubParsersAction(Action):
         # values from the subnamespace.
         subnamespace_name = getattr(subparser, 'subnamespace', None)
         if subnamespace_name is not None:
+            subnamespace_name = subnamespace_name.replace('-', '_')
             setattr(namespace, subnamespace_name, subnamespace)
         else:
             for key, value in vars(subnamespace).items():
